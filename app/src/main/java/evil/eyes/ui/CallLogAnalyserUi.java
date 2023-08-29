5c1a6215d3bed79139700539e068143eaaf7ca15
hrishikeshprateek <prateekhrishikesh@gmail.com>
2023-08-29 22:05:21 +0530

    #1.7 docs

--- app/src/main/java/evil/eyes/ui/CallLogAnalyserUi.java
+++ app/src/main/java/evil/eyes/ui/CallLogAnalyserUi.java
@@ -3,18 +3,21 @@ package evil.eyes.ui;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import evil.eyes.R;
 import thundersharp.sensivisionhealth.loganalyzer.annos.ArrangeBy;
 import thundersharp.sensivisionhealth.loganalyzer.annos.OperationModes;
 import thundersharp.sensivisionhealth.loganalyzer.asyncs.CallLogsAnalyzer;
+import thundersharp.sensivisionhealth.loganalyzer.constants.JSONConstants;
 import thundersharp.sensivisionhealth.loganalyzer.errors.AnalyzeException;
 import thundersharp.sensivisionhealth.loganalyzer.interfaces.OnCallLogsAnalyzed;
 
@@ -24,6 +27,7 @@ public class CallLogAnalyserUi extends AppCompatActivity {
     private SharedPreferences sharedPreferences;
     private ProgressBar progressBar;
     private boolean f = false;
+    private TextView entries_log,tv,sortBy,talkTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,45 +35,59 @@ public class CallLogAnalyserUi extends AppCompatActivity {
         setContentView(R.layout.activity_call_log_analyser_ui);
 
         timeStamp = getIntent().getStringExtra("data");
-        sharedPreferences = getSharedPreferences("data",MODE_PRIVATE);
-        String data = sharedPreferences.getString(timeStamp,null);
+        sharedPreferences = getSharedPreferences("data", MODE_PRIVATE);
+        String data = sharedPreferences.getString(timeStamp, null);
         if (timeStamp == null || data == null) finish();
 
-        progressBar =findViewById(R.id.progressBar);
-        TextView tv = findViewById(R.id.tvw);
+        progressBar = findViewById(R.id.progressBar);
+        entries_log = findViewById(R.id.entries_log);
+        tv = findViewById(R.id.tvw);
+        sortBy = findViewById(R.id.sortBy);
+        talkTime = findViewById(R.id.talkTime);
 
-        new Handler().postDelayed(()->{
-            CallLogsAnalyzer
-                    .getCallLogsAnalyzer()
-                    .setOperationMode(OperationModes.basicAnalyze)
-                    .setArrangeBy(ArrangeBy.duration)
-                    .setOnCallLogsAnalyzedListener(new OnCallLogsAnalyzed() {
-                        @Override
-                        public void onExtractionSuccessFull(JSONObject data) {
-                            runOnUiThread(()->tv.setText("complete..."));
-                        }
+        new Handler().postDelayed(() ->
+                CallLogsAnalyzer
+                        .getCallLogsAnalyzer()
+                        .setOperationMode(OperationModes.basicAnalyze)
+                        .setArrangeBy(ArrangeBy.duration)
+                        .setOnCallLogsAnalyzedListener(new OnCallLogsAnalyzed() {
+                            @Override
+                            public void onExtractionSuccessFull(JSONObject data) {
+                                runOnUiThread(() -> {
+                                    try {
+                                        updateUI(data);
+                                    } catch (JSONException e) {
+                                        e.printStackTrace();
+                                    }
+                                });
+                            }
 
-                        @Override
-                        public void onFailedToAnalyze(AnalyzeException analyzeException) {
-                            runOnUiThread(()-> Toast.makeText(CallLogAnalyserUi.this, analyzeException.getMessage(), Toast.LENGTH_SHORT).show());
-                        }
+                            @Override
+                            public void onFailedToAnalyze(AnalyzeException analyzeException) {
+                                runOnUiThread(() -> Toast.makeText(CallLogAnalyserUi.this, analyzeException.getMessage(), Toast.LENGTH_SHORT).show());
+                            }
 
-                        @Override
-                        public void onProgress(int processed, long total) {
-                            runOnUiThread(()->{
-                                if (!f){
-                                    progressBar.setMax((int) total);
-                                    f = true;
-                                }
-                                progressBar.setProgress(processed);
-                                tv.setText("Processed "+processed+" of "+total+" entries");
-                            });
+                            @Override
+                            public void onProgress(int processed, long total) {
+                                runOnUiThread(() -> {
+                                    if (!f) {
+                                        progressBar.setMax((int) total);
+                                        entries_log.setText((int) total+" entries");
+                                        f = true;
+                                    }
+                                    progressBar.setProgress(processed);
+                                    tv.setText("Processed " + processed + " of " + total + " entries");
+                                });
 
-                        }
-                    }).execute(data);
+                            }
+                        }).execute(data), 1000);
 
-        },1000);
+    }
 
+    private void updateUI(JSONObject data) throws JSONException {
+        tv.setText("Completed...");
+        sortBy.setText("Call "+data.getString(JSONConstants.QUERY_TYPE).toLowerCase());
+        talkTime.setText("> "+data.getString(JSONConstants.TOTAL_TALK_TIME).substring(0,data.getString(JSONConstants.TOTAL_TALK_TIME).indexOf("h"))+"hrs");
     }
 
     @Override
@@ -79,5 +97,9 @@ public class CallLogAnalyserUi extends AppCompatActivity {
         SharedPreferences.Editor editor = sharedPreferences.edit();
         editor.remove(timeStamp);
         editor.apply();
+    }
+
+    public void close(View view) {
+        finish();
     }
 }
