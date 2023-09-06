df66375db1691b84b1a89b4dd4e2510b81be9ac0
hrishikeshprateek <prateekhrishikesh@gmail.com>
2023-09-06 11:48:32 +0530

    #1.7 Ui implementation for CallLogAnalyserUi.java

--- app/src/main/java/evil/eyes/ui/IndivisualAnalysis.java
+++ app/src/main/java/evil/eyes/ui/IndivisualAnalysis.java
@@ -4,6 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.anychart.AnyChart;
+import com.anychart.AnyChartView;
+import com.anychart.chart.common.dataentry.DataEntry;
+import com.anychart.chart.common.dataentry.ValueDataEntry;
+import com.anychart.chart.common.listener.Event;
+import com.anychart.chart.common.listener.ListenersInterface;
+import com.anychart.charts.Pie;
+import com.anychart.enums.Align;
+import com.anychart.enums.LegendLayout;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import evil.eyes.R;
 import thundersharp.sensivisionhealth.loganalyzer.annos.OperationModes;
@@ -23,12 +38,58 @@ public class IndivisualAnalysis extends AppCompatActivity {
         number = getIntent().getStringExtra("phone");
 
         sharedPreferences = getSharedPreferences("data", MODE_PRIVATE);
-        String data = sharedPreferences.getString(timeStamp, null);
-        if (timeStamp == null || data == null || number == null) finish();
+        String dataTrans = sharedPreferences.getString(timeStamp, null);
+        if (timeStamp == null || dataTrans == null || number == null) finish();
 
         CallLogsAnalyzer
                 .getCallLogsAnalyzer()
                 .setOperationMode(OperationModes.queryByNumber)
                 .setQueryPhoneNo(number);
+        generateGraph();
+
+        AnyChartView anyChartView = findViewById(R.id.any_chart_view);
+        anyChartView.setProgressBar(findViewById(R.id.progressBar));
+
+        Pie pie = AnyChart.pie();
+
+        pie.setOnClickListener(new ListenersInterface.OnClickListener(new String[]{"x", "value"}) {
+            @Override
+            public void onClick(Event event) {
+                Toast.makeText(IndivisualAnalysis.this, event.getData().get("x") + ":" + event.getData().get("value"), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        List<DataEntry> data = new ArrayList<>();
+        data.add(new ValueDataEntry("Apples", 6371664));
+        data.add(new ValueDataEntry("Pears", 789622));
+        data.add(new ValueDataEntry("Bananas", 7216301));
+        data.add(new ValueDataEntry("Grapes", 1486621));
+        data.add(new ValueDataEntry("Oranges", 1200000));
+
+        pie.data(data);
+
+        pie.title("Fruits imported in 2015 (in kg)");
+
+        pie.labels().position("outside");
+
+        pie.legend().title().enabled(true);
+        pie.legend().title()
+                .text("Retail channels")
+                .padding(0d, 0d, 10d, 0d);
+
+        pie.legend()
+                .position("center-bottom")
+                .itemsLayout(LegendLayout.HORIZONTAL)
+                .align(Align.CENTER);
+
+        anyChartView.setChart(pie);
+    }
+
+    public void close(View view) {
+        finish();
+    }
+
+    private void generateGraph(){
+
     }
 }
